d6e1691066fde7d94845bf8f443ec999acf55aaa
Nguyen Van Quy <142620727+nvqq10102005@users.noreply.github.com>
2024-12-23 11:07:06 +0700

    "Using JPA and ORM"

--- src/main/java/vn/com/LaptopShop/domain/User.java
+++ src/main/java/vn/com/LaptopShop/domain/User.java
@@ -1,7 +1,14 @@
 package vn.com.LaptopShop.domain;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id ;
 
     private String email ;
