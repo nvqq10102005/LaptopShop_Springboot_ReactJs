67e1ffa43b46fe0fe20837d6751b062b362bc22e
Nguyen Van Quy <142620727+nvqq10102005@users.noreply.github.com>
2024-12-23 14:27:17 +0700

    "implement class Repository "

--- src/main/java/vn/com/LaptopShop/controller/UserController.java
+++ src/main/java/vn/com/LaptopShop/controller/UserController.java
@@ -7,20 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.com.LaptopShop.domain.User;
+import vn.com.LaptopShop.repository.UserRepository;
 import vn.com.LaptopShop.service.UserService;
 
 @Controller
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService
+    ) {
         this.userService = userService;
+
     }
 
     @RequestMapping("/")
     public String getView(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("hello", test);
+        model.addAttribute("hello", "test");
         model.addAttribute("hoidanit", "from controller");
         return "hello";
     }
@@ -28,12 +30,14 @@ public class UserController {
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
         model.addAttribute("newUser", new User());
+        
         return "admin/user/create";
     }
 
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
-        System.out.println(" run here " + hoidanit);
+    public String createUserPage(Model model, @ModelAttribute("newUser") User user) {
+        System.out.println(" run here " + user);
+        this.userService.handlesaveUser(user);
         return "hello";
     }
 }
--- src/main/java/vn/com/LaptopShop/service/UserService.java
+++ src/main/java/vn/com/LaptopShop/service/UserService.java
@@ -2,10 +2,23 @@ package vn.com.LaptopShop.service;
 
 import org.springframework.stereotype.Service;
 
+import vn.com.LaptopShop.domain.User;
+import vn.com.LaptopShop.repository.UserRepository;
+
 @Service
 public class UserService {
+    private final UserRepository userRepository;
 
-    public String handleHello(){
-        return "hello from Service";
+    
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
+    public User handlesaveUser(User user){
+        User getUser = this.userRepository.save(user);
+        return getUser;
     }
 }
