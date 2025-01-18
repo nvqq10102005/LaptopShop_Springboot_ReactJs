a92233bc649aa682b720aea78bba3ed0abdbdf7d
Nguyen Van Quy <142620727+nvqq10102005@users.noreply.github.com>
2025-01-18 12:51:16 +0700

    "config session and remember me"

--- src/main/java/vn/com/LaptopShop/config/SecurityConfiguration.java
+++ src/main/java/vn/com/LaptopShop/config/SecurityConfiguration.java
@@ -1,15 +1,19 @@
 package vn.com.LaptopShop.config;
 
+import javax.swing.Spring;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
 import jakarta.servlet.DispatcherType;
 import vn.com.LaptopShop.service.CustomUserDetailsService;
@@ -44,6 +48,12 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public SpringSessionRememberMeServices rememberMeServices() {
+        SpringSessionRememberMeServices rememberMeServices = new SpringSessionRememberMeServices();
+        rememberMeServices.setAlwaysRemember(true);
+        return rememberMeServices;
+    }
+    @Bean
     public AuthenticationSuccessHandler successHandler() {
         return new CustomSuccessHandler();
     }
@@ -58,7 +68,13 @@ public class SecurityConfiguration {
                 .requestMatchers("/","/login","register", "/client/**","/product/**", "/css/**", "/js/**", "/images/**").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                   .anyRequest().authenticated())
-
+                  .sessionManagement((sessionManagement) -> sessionManagement
+		.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+	            .invalidSessionUrl("/logout?expired")
+	            .maximumSessions(1)
+	            .maxSessionsPreventsLogin(false))
+	            .logout(logout->logout.deleteCookies("JSESSIONID").invalidateHttpSession(true))
+                .rememberMe(r -> r.rememberMeServices(rememberMeServices()))
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login-account")
                         .loginProcessingUrl("/perform-login")
